7cf28c21e553dc31e938f308d06413f269ee3906
dentrodeunmensaje <dentrodeunmensaje@hotmail.com>
2022-06-09 14:03:06 -0600

    finished homework 03

--- 03-WebServer/homework/showByName/app.js
+++ 03-WebServer/homework/showByName/app.js
@@ -1,4 +1,17 @@
-var fs  = require("fs")
-var http  = require("http")
+var fs = require("fs");
+var http = require("http");
 
 // Escribí acá tu servidor
+http
+  .createServer((req, res) => {
+      fs.readFile(`${__dirname}/${req.url}.jpg`, (err, data) => {
+        if (err) {
+          res.writeHead(404, { "Content-Type": "text/plain" });
+          res.end("Error");
+        } else {
+          res.writeHead(200, { "Content-Type": "image/jpeg" });
+          res.end(data);
+        }
+      });
+  })
+  .listen(8080, "127.0.0.1");
